3859b87059913a211d08be87e4cd624e149daaec
ghidella <raphaelfghidella@gmail.com>
2023-07-06 20:28:16 -0300

    code update

--- valor.cpp
+++ valor.cpp
@@ -1,47 +1,52 @@
-#include <iostream>
+#include <iostream> // Importação de biblioteca para uso de entrada e saída de dados.
 
-void valor(int x, int& Fx, int& Gx) {
-    if (x == 1) {
-        Fx = 2;
-        Gx = 1;
+void valor(int k, int &Fk, int &Gk) // Declara função valor, que recebe como parâmetros o inteiro k, e os ponteiros de inteiro Fk e Gk
+{
+    if (k == 1) // Verifica se k = 1 é verdadeiro, se for associa os valores 2 e 1 para as variáveis Fk e Gk respectivamente
+    {
+        Fk = 2;
+        Gk = 1;
         return;
     }
 
-    if (x == 2) {
-        Fx = 1;
-        Gx = 2;
+    if (k == 2) // Verifica se k = 2 é verdadeiro, se for associa os valores 1 e 2 para as variáveis Fk e Gk respectivamente.
+    {
+        Fk = 1;
+        Gk = 2;
         return;
     }
 
-    int Fi_minus_1 = 1, Fi_minus_2 = 2;
-    int Gi_minus_1 = 2, Gi_minus_2 = 1;
+    int Fi_menos_1 = 1, Fi_menos_2 = 2;
+    int Gi_menos_1 = 2, Gi_menos_2 = 1;
 
-    for (int i = 3; i <= x; i++) {
-        int Fi = 2 * Fi_minus_1 + Gi_minus_2;
-        int Gi = Gi_minus_1 + 3 * Fi_minus_2;
+    for (int i = 3; i <= k; i++)
+    {
+        int Fi = 2 * Fi_menos_1 + Gi_menos_2;
+        int Gi = Gi_menos_1 + 3 * Fi_menos_2;
 
-        Fi_minus_2 = Fi_minus_1;
-        Fi_minus_1 = Fi;
+        Fi_menos_2 = Fi_menos_1;
+        Fi_menos_1 = Fi;
 
-        Gi_minus_2 = Gi_minus_1;
-        Gi_minus_1 = Gi;
+        Gi_menos_2 = Gi_menos_1;
+        Gi_menos_1 = Gi;
     }
 
-    Fx = Fi_minus_1;
-    Gx = Gi_minus_1;
+    Fk = Fi_menos_1;
+    Gk = Gi_menos_1;
 }
 
-int main() {
-    int x;
+int main()
+{
+    int k;
 
-    std::cout << "Digite o valor de x: ";
-    std::cin >> x;
+    std::cout << "Digite o valor de k: ";
+    std::cin >> k;
 
-    int Fx, Gx;
-    valor(x, Fx, Gx);
+    int Fk, Gk;
+    valor(k, Fk, Gk);
 
-    std::cout << "Fx = " << Fx << std::endl;
-    std::cout << "Gx = " << Gx << std::endl;
+    std::cout << "F(" << k << ") = " << Fk << std::endl;
+    std::cout << "G(" << k << ") = "<< Gk << std::endl;
 
     return 0;
 }
